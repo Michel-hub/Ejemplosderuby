9f7f26b1d1e898764fc7f6f6dc2fb843d509fa57
Michel-hub <mc.ubilla93@gmail.com>
2020-05-23 22:44:28 -0400

    cuartocomando

--- letra_x.rb
+++ letra_x.rb
@@ -3,9 +3,13 @@ def letrax(n)
 
     n.times do|i|
         if i==0 || i == (n-1)
-            print "*****\n"
-        else 
+            print "*   *\n"
+        elsif i==1
+            print " * * \n"
+        elsif i==2
             print "  *  \n"
+        else i==3
+            print " * * \n"
         end
       end 
     end
